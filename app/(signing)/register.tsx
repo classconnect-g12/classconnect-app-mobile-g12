b473c54f502af74adc95255c298df2a665cd89d7
S2JuanS2 <juansdelrio@hotmail.com>
2025-03-30 16:37:46 -0300

    feat: add register view

---
+++ app/(signing)/register.tsx
@@ -0,0 +1,83 @@
+import { Link } from "expo-router";
+import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
+import { TextInput } from "react-native-paper";
+
+export default function SignUp() {
+  return (
+    <View style={styles.container}>
+      {/* <Image source={require('../../assets/images/classconnect-logo.png')} style={styles.logo} resizeMode="contain"/> */}
+      <Text style={styles.title}>Create Account</Text>
+      <Text style={styles.subtitle}>Sign up to get started</Text>
+      <TextInput
+        style={styles.input}
+        label="Email"
+        mode="outlined"
+        theme={{ colors: { primary: "#2b9dd6" } }}
+      />
+      <TextInput
+        style={styles.input}
+        secureTextEntry
+        label="Password"
+        mode="outlined"
+        theme={{ colors: { primary: "#2b9dd6" } }}
+      />
+      <TextInput
+        style={styles.input}
+        secureTextEntry
+        label="Confirm Password"
+        mode="outlined"
+        theme={{ colors: { primary: "#2b9dd6" } }}
+      />
+      <TouchableOpacity style={styles.button}>
+        <Text style={{ color: "#FFF" }}>Sign Up</Text>
+      </TouchableOpacity>
+      <Text style={styles.footerText}>
+        Already have an account?
+        <Link href="/login">Sign in</Link>
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+    backgroundColor: "#FFF",
+  },
+  logo: {
+    width: 800,
+    height: 100,
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 16,
+    marginBottom: 20,
+  },
+  input: {
+    width: "80%",
+    height: 40,
+    paddingHorizontal: 10,
+    marginBottom: 15,
+  },
+  footerText: {
+    marginTop: 20,
+    fontSize: 14,
+    color: "blue",
+  },
+  button: {
+    width: "30%",
+    alignItems: "center",
+    backgroundColor: "#2b9dd6",
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 10,
+  },
+});
