5cd0321f57958d5e7dbd62708ea66548d3deeeb4
lTomaas <psilva@fi.uba.ar>
2025-05-13 07:50:35 -0300

    feat(members): getAcceptedMembers endpoint to fetch members

--- src/services/EnrollmentService.ts
+++ src/services/EnrollmentService.ts
@@ -10,3 +10,8 @@ export async function getMyEnrollments(page = 0, limit = 10) {
 export async function enrollInCourse(courseId: string) {
   await privateClient.post(`/enrollment/create/${courseId}`);
 }
+
+export const getAcceptedMembers = async (courseId: string, page = 0, limit = 10) => {
+  const response = await privateClient.get(`/enrollment/pendings/${courseId}?page=${page}&limit=${limit}`);
+  return response.data;
+};
