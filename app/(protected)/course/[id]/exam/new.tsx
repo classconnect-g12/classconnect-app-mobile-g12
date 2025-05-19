0b1bfd3959905cd11ea8f11abf13f0d55ab54a0e
Valentin <vcalomino@fi.uba.ar>
2025-05-19 19:45:53 -0300

    style: improve exam creation form styles

--- app/(protected)/course/[id]/exam/new.tsx
+++ app/(protected)/course/[id]/exam/new.tsx
@@ -1,5 +1,10 @@
 import { useState } from "react";
-import { View, StyleSheet, Pressable, ScrollView } from "react-native";
+import {
+  View,
+  Pressable,
+  ScrollView,
+  KeyboardAvoidingView,
+} from "react-native";
 import {
   TextInput,
   Button,
@@ -16,7 +21,6 @@ import {
   AssesmentQuestion,
   AssesmentType,
   createAssessment,
-  QuestionType,
 } from "@services/AssesmentService";
 import { DateTimePickerAndroid } from "@react-native-community/datetimepicker";
 import { SNACKBAR_VARIANTS } from "@constants/snackbarVariants";
@@ -24,6 +28,9 @@ import { handleApiError } from "@utils/handleApiError";
 import { Image } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import { AppSnackbar } from "@components/AppSnackbar";
+import { createCourseStyles as styles } from "@styles/createCourseStyles";
+import { colors } from "@theme/colors";
+import { Picker } from "@react-native-picker/picker";
 
 const defaultQuestion = {
   text: "",
@@ -89,10 +96,6 @@ export default function NewExamScreen() {
   };
 
   const handleSubmit = async () => {
-    console.log("Pressed");
-
-    console.log(courseId);
-
     if (!title || !instructions || questions.length === 0) {
       showSnackbar(
         "Por favor completa todos los campos.",
@@ -141,242 +144,258 @@ export default function NewExamScreen() {
   };
 
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <TextInput
-        label="Título del examen"
-        value={title}
-        onChangeText={setTitle}
-        style={styles.input}
-      />
-      <TextInput
-        label="Instrucciones"
-        value={instructions}
-        onChangeText={setInstructions}
-        multiline
-        numberOfLines={4}
-        style={styles.input}
-      />
-      <TextInput
-        label="Puntaje máximo"
-        keyboardType="numeric"
-        value={maxScore}
-        onChangeText={setMaxScore}
-        style={styles.input}
-      />
-      <TextInput
-        label="Puntaje mínimo"
-        keyboardType="numeric"
-        value={minScore}
-        onChangeText={setMinScore}
-        style={styles.input}
-      />
-      <TextInput
-        label="Minutos de tolerancia"
-        keyboardType="numeric"
-        value={gracePeriodMinutes}
-        onChangeText={setGracePeriodMinutes}
-        style={styles.input}
-      />
-      <TextInput
-        label="Penalización por entrega tardía (%)"
-        keyboardType="numeric"
-        value={latePenaltyPercentage}
-        onChangeText={setLatePenaltyPercentage}
-        style={styles.input}
-      />
-      <View style={styles.switchRow}>
-        <Text>Permitir entregas tardías</Text>
-        <Switch
-          value={allowLateSubmission}
-          onValueChange={setAllowLateSubmission}
-        />
-      </View>
-
-      <Text style={styles.label}>Fecha de inicio:</Text>
-      <Pressable
-        onPress={() => {
-          DateTimePickerAndroid.open({
-            value: startDate,
-            mode: "date",
-            is24Hour: true,
-            onChange: (event, selectedDate) => {
-              if (event.type === "set" && selectedDate) {
-                setStartDate(selectedDate);
-              }
-            },
-          });
-        }}
-      >
-        <TextInput
-          value={startDate.toLocaleString()}
-          editable={false}
-          style={styles.input}
-          pointerEvents="none"
-        />
-      </Pressable>
-
-      <Text style={styles.label}>Fecha de fin:</Text>
-      <Pressable
-        onPress={() => {
-          DateTimePickerAndroid.open({
-            value: endDate,
-            mode: "date",
-            is24Hour: true,
-            onChange: (event, selectedDate) => {
-              if (event.type === "set" && selectedDate) {
-                setEndDate(selectedDate);
-              }
-            },
-          });
-        }}
-      >
-        <TextInput
-          value={endDate.toLocaleString()}
-          editable={false}
-          style={styles.input}
-          pointerEvents="none"
-        />
-      </Pressable>
-
-      <Text style={styles.label}>Preguntas</Text>
-      {questions.map((q, index) => (
-        <View key={index} style={styles.questionBox}>
+    <KeyboardAvoidingView style={styles.container} behavior="height">
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <View style={styles.inputContainer}>
           <TextInput
-            label={`Pregunta #${index + 1}`}
-            value={q.text}
-            onChangeText={(text) => handleQuestionChange(index, "text", text)}
+            label="Título del examen"
+            value={title}
+            onChangeText={setTitle}
+            style={styles.input}
+            theme={{ colors: { primary: colors.primary } }}
+          />
+          <TextInput
+            theme={{ colors: { primary: colors.primary } }}
+            label="Instrucciones"
+            value={instructions}
+            onChangeText={setInstructions}
+            multiline
+            numberOfLines={4}
             style={styles.input}
           />
           <TextInput
-            label="Puntaje"
+            theme={{ colors: { primary: colors.primary } }}
+            label="Puntaje máximo"
             keyboardType="numeric"
-            value={String(q.score)}
-            onChangeText={(score) =>
-              handleQuestionChange(index, "score", Number(score))
-            }
+            value={maxScore}
+            onChangeText={setMaxScore}
             style={styles.input}
           />
           <TextInput
-            label="Tipo"
-            value={q.type}
-            onChangeText={(value) => handleQuestionChange(index, "type", value)}
+            theme={{ colors: { primary: colors.primary } }}
+            label="Puntaje mínimo"
+            keyboardType="numeric"
+            value={minScore}
+            onChangeText={setMinScore}
             style={styles.input}
           />
-          {q.type === "MULTIPLE_CHOICE" && (
-            <>
-              {q.options?.map((opt, i) => (
+          <TextInput
+            theme={{ colors: { primary: colors.primary } }}
+            label="Minutos de tolerancia"
+            keyboardType="numeric"
+            value={gracePeriodMinutes}
+            onChangeText={setGracePeriodMinutes}
+            style={styles.input}
+          />
+          <TextInput
+            theme={{ colors: { primary: colors.primary } }}
+            label="Penalización por entrega tardía (%)"
+            keyboardType="numeric"
+            value={latePenaltyPercentage}
+            onChangeText={setLatePenaltyPercentage}
+            style={styles.input}
+          />
+          <View style={styles.switchRow}>
+            <Text>Permitir entregas tardías</Text>
+            <Switch
+              value={allowLateSubmission}
+              onValueChange={setAllowLateSubmission}
+            />
+          </View>
+
+          <Text style={styles.label}>Fecha de inicio:</Text>
+          <Pressable
+            onPress={() => {
+              DateTimePickerAndroid.open({
+                value: startDate,
+                mode: "date",
+                is24Hour: true,
+                onChange: (event, selectedDate) => {
+                  if (event.type === "set" && selectedDate) {
+                    setStartDate(selectedDate);
+                  }
+                },
+              });
+            }}
+          >
+            <TextInput
+              theme={{ colors: { primary: colors.primary } }}
+              value={startDate.toLocaleString()}
+              editable={false}
+              style={styles.input}
+              pointerEvents="none"
+            />
+          </Pressable>
+
+          <Text style={styles.label}>Fecha de fin:</Text>
+          <Pressable
+            onPress={() => {
+              DateTimePickerAndroid.open({
+                value: endDate,
+                mode: "date",
+                is24Hour: true,
+                onChange: (event, selectedDate) => {
+                  if (event.type === "set" && selectedDate) {
+                    setEndDate(selectedDate);
+                  }
+                },
+              });
+            }}
+          >
+            <TextInput
+              theme={{ colors: { primary: colors.primary } }}
+              value={endDate.toLocaleString()}
+              editable={false}
+              style={styles.input}
+              pointerEvents="none"
+            />
+          </Pressable>
+
+          <Text style={styles.label}>Preguntas</Text>
+          <View style={styles.inputContainer}>
+            {questions.map((q, index) => (
+              <View key={index} style={styles.questionBox}>
                 <TextInput
-                  key={i}
-                  label={`Opción ${String.fromCharCode(65 + i)}`}
-                  value={opt}
-                  onChangeText={(text) => handleOptionChange(index, i, text)}
+                  theme={{ colors: { primary: colors.primary } }}
+                  label={`Pregunta #${index + 1}`}
+                  value={q.text}
+                  onChangeText={(text) =>
+                    handleQuestionChange(index, "text", text)
+                  }
                   style={styles.input}
                 />
-              ))}
-              <TextInput
-                label="Opción correcta (A, B, C, D)"
-                value={q.correctOption}
-                onChangeText={(text) =>
-                  handleQuestionChange(index, "correctOption", text)
-                }
-                style={styles.input}
-              />
-            </>
-          )}
-          {q.type === "WRITTEN_ANSWER" && (
-            <TextInput
-              label="Respuesta de ejemplo"
-              value={q.sampleAnswer}
-              onChangeText={(text) =>
-                handleQuestionChange(index, "sampleAnswer", text)
-              }
-              style={styles.input}
-            />
-          )}
-          <View style={styles.switchRow}>
-            <Text>¿Tiene imagen?</Text>
-            <Switch
-              value={q.hasImage}
-              onValueChange={(value) =>
-                handleQuestionChange(index, "hasImage", value)
-              }
-            />
-            {q.hasImage && (
-              <>
-                <Button
-                  mode="outlined"
-                  onPress={() => handlePickImage(index)}
-                  style={{ marginVertical: 8 }}
-                >
-                  Seleccionar imagen
-                </Button>
-                {q.imageUri && (
-                  <Image
-                    source={{ uri: q.imageUri }}
-                    style={{
-                      width: "100%",
-                      height: 200,
-                      marginTop: 8,
-                      borderRadius: 8,
-                    }}
-                    resizeMode="contain"
+                <TextInput
+                  theme={{ colors: { primary: colors.primary } }}
+                  label="Puntaje"
+                  keyboardType="numeric"
+                  value={String(q.score)}
+                  onChangeText={(score) =>
+                    handleQuestionChange(index, "score", Number(score))
+                  }
+                  style={styles.input}
+                />
+                <View style={styles.input}>
+                  <Picker
+                    selectedValue={q.type}
+                    onValueChange={(value) =>
+                      handleQuestionChange(index, "type", value)
+                    }
+                    style={{ color: colors.text }}
+                  >
+                    <Picker.Item
+                      label="Multiple Choice"
+                      value="MULTIPLE_CHOICE"
+                    />
+                    <Picker.Item
+                      label="Written answer"
+                      value="WRITTEN_ANSWER"
+                    />
+                    <Picker.Item
+                      label="File attachment"
+                      value="FILE_ATTACHMENT"
+                    />
+                  </Picker>
+                </View>
+
+                {q.type === "MULTIPLE_CHOICE" && (
+                  <>
+                    {q.options?.map((opt, i) => (
+                      <TextInput
+                        theme={{ colors: { primary: colors.primary } }}
+                        key={i}
+                        label={`Opción ${String.fromCharCode(65 + i)}`}
+                        value={opt}
+                        onChangeText={(text) =>
+                          handleOptionChange(index, i, text)
+                        }
+                        style={styles.input}
+                      />
+                    ))}
+                    <TextInput
+                      theme={{ colors: { primary: colors.primary } }}
+                      label="Opción correcta (A, B, C, D)"
+                      value={q.correctOption}
+                      onChangeText={(text) =>
+                        handleQuestionChange(index, "correctOption", text)
+                      }
+                      style={styles.input}
+                    />
+                  </>
+                )}
+                {q.type === "WRITTEN_ANSWER" && (
+                  <TextInput
+                    theme={{ colors: { primary: colors.primary } }}
+                    label="Respuesta de ejemplo"
+                    value={q.sampleAnswer}
+                    onChangeText={(text) =>
+                      handleQuestionChange(index, "sampleAnswer", text)
+                    }
+                    style={styles.input}
                   />
                 )}
-              </>
-            )}
+                <View style={styles.switchRow}>
+                  <Text>¿Tiene imagen?</Text>
+                  <Switch
+                    value={q.hasImage}
+                    onValueChange={(value) =>
+                      handleQuestionChange(index, "hasImage", value)
+                    }
+                  />
+                  {q.hasImage && (
+                    <>
+                      <Button
+                        mode="outlined"
+                        onPress={() => handlePickImage(index)}
+                        style={{ marginVertical: 8 }}
+                      >
+                        Seleccionar imagen
+                      </Button>
+                      {q.imageUri && (
+                        <Image
+                          source={{ uri: q.imageUri }}
+                          style={{
+                            width: "100%",
+                            height: 200,
+                            marginTop: 8,
+                            borderRadius: 8,
+                          }}
+                          resizeMode="contain"
+                        />
+                      )}
+                    </>
+                  )}
+                </View>
+                <IconButton
+                  icon="delete"
+                  onPress={() => removeQuestion(index)}
+                />
+              </View>
+            ))}
           </View>
-          <IconButton icon="delete" onPress={() => removeQuestion(index)} />
+
+          <Button mode="contained" onPress={addQuestion} style={styles.button}>
+            Agregar pregunta
+          </Button>
+
+          {loading ? (
+            <ActivityIndicator style={{ marginTop: 16 }} />
+          ) : (
+            <Button
+              mode="contained"
+              onPress={handleSubmit}
+              style={styles.button}
+            >
+              Crear examen
+            </Button>
+          )}
+          <AppSnackbar
+            visible={snackbarVisible}
+            message={snackbarMessage}
+            onDismiss={hideSnackbar}
+            variant={snackbarVariant}
+          />
         </View>
-      ))}
-
-      <Button mode="outlined" onPress={addQuestion} style={styles.button}>
-        Agregar pregunta
-      </Button>
-
-      {loading ? (
-        <ActivityIndicator style={{ marginTop: 16 }} />
-      ) : (
-        <Button mode="contained" onPress={handleSubmit} style={styles.button}>
-          Crear examen
-        </Button>
-      )}
-      <AppSnackbar
-        visible={snackbarVisible}
-        message={snackbarMessage}
-        onDismiss={hideSnackbar}
-        variant={snackbarVariant}
-      />
-    </ScrollView>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 16,
-    flexGrow: 1,
-  },
-  input: {
-    marginBottom: 16,
-  },
-  label: {
-    marginTop: 16,
-    marginBottom: 8,
-    fontWeight: "bold",
-  },
-  switchRow: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    marginVertical: 8,
-  },
-  questionBox: {
-    borderWidth: 1,
-    borderColor: "#ccc",
-    padding: 12,
-    marginVertical: 8,
-    borderRadius: 8,
-  },
-  button: {
-    marginTop: 16,
-  },
-});
