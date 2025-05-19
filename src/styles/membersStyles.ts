6fb5755005e23b440f4a9d2119ef7f5a6d933c29
S2JuanS2 <juansdelrio@hotmail.com>
2025-05-19 20:31:47 -0300

    feat: add permissions and add remove students

--- src/styles/membersStyles.ts
+++ src/styles/membersStyles.ts
@@ -65,4 +65,92 @@ export const membersStyles = StyleSheet.create({
     borderTopLeftRadius: 8,
     borderTopRightRadius: 8,
   },
+  actions: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginLeft: "auto",
+  },
+  modalContainer: {
+    backgroundColor: "white",
+    padding: 20,
+    marginHorizontal: 30,
+    borderRadius: 16,
+    elevation: 4,
+    maxHeight: "80%",
+  },
+  modalOverlay: {
+    flex: 1,
+    backgroundColor: "rgba(0, 0, 0, 0.4)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  modalBox: {
+    backgroundColor: "white",
+    borderRadius: 16,
+    padding: 20,
+    elevation: 6,
+    width: "85%",
+    alignSelf: "center",
+  },
+
+  modalTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 12,
+    textAlign: "center",
+  },
+  modalSubTitle: {
+    fontSize: 16,
+    marginBottom: 12,
+    textAlign: "center",
+  },
+  scrollContainer: {
+    borderTopLeftRadius: 6,
+    borderTopRightRadius: 6,
+    borderWidth: 1,
+    borderColor: "gray",
+    maxHeight: 250,
+  },
+
+  permissionItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 4,
+  },
+
+  permissionLabel: {
+    fontSize: 14,
+  },
+  confirmButton: {
+    marginLeft: 8,
+    borderRadius: 6,
+    backgroundColor: colors.primary,
+    marginBottom: 10,
+  },
+  cancelButton: {
+    borderRadius: 6,
+    backgroundColor: colors.background,
+    marginBottom: 10,
+  },
+  confirmModalBox: {
+    backgroundColor: "white",
+    marginHorizontal: 30,
+    padding: 20,
+    borderRadius: 8,
+    elevation: 5,
+  },
+  selectButton: {
+    borderTopWidth: 0,
+    marginBottom: 8,
+    borderTopRightRadius: 0,
+    borderTopLeftRadius: 0,
+    borderRadius: 6,
+  },
+  confirmButtonRemove: {
+    marginLeft: 8,
+    borderRadius: 6,
+    backgroundColor: "#d32f2f",
+    marginBottom: 10,
+  },
 });
