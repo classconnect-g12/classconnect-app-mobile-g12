8a79c074bb51cffbb3593c7b2ab6b46ae6b1b75b
Valentin <vcalomino@fi.uba.ar>
2025-05-21 12:22:31 -0300

    feat: add field validation to edit exam

--- app/(protected)/course/[id]/exam/editExam/[examId].tsx
+++ app/(protected)/course/[id]/exam/editExam/[examId].tsx
@@ -164,7 +164,87 @@ export default function EditExamScreen() {
   };
 
   const handleSubmit = async () => {
-    // validaciones iguales que en NewExamScreen...
+    if (!title) {
+      showSnackbar("Please complete the exam title", SNACKBAR_VARIANTS.ERROR);
+      return;
+    }
+
+    if (!instructions) {
+      showSnackbar(
+        "Please complete the exam instructions",
+        SNACKBAR_VARIANTS.ERROR
+      );
+      return;
+    }
+
+    if (parseInt(maxScore) <= 0) {
+      showSnackbar(
+        "Max score can't be less or equal than zero",
+        SNACKBAR_VARIANTS.ERROR
+      );
+      return;
+    }
+
+    if (parseInt(minScore) < 0) {
+      showSnackbar(
+        "Min score can't be less or equal than zero",
+        SNACKBAR_VARIANTS.ERROR
+      );
+      return;
+    }
+
+    if (parseInt(maxScore) < parseInt(minScore)) {
+      showSnackbar(
+        "Max score can't be smaller than min score",
+        SNACKBAR_VARIANTS.ERROR
+      );
+      return;
+    }
+
+    if (parseInt(gracePeriodMinutes) < 0) {
+      showSnackbar(
+        "Tolerance minutes can't be less than zero",
+        SNACKBAR_VARIANTS.ERROR
+      );
+      return;
+    }
+
+    if (parseFloat(latePenaltyPercentage) < 0.0) {
+      showSnackbar(
+        "Late delivery pentalty can't be less than zero",
+        SNACKBAR_VARIANTS.ERROR
+      );
+      return;
+    }
+
+    if (startDate >= endDate) {
+      showSnackbar(
+        "End date must be after start date",
+        SNACKBAR_VARIANTS.ERROR
+      );
+      return;
+    }
+
+    if (questions.length === 0) {
+      showSnackbar(
+        "Please add at least one question to the exam",
+        SNACKBAR_VARIANTS.ERROR
+      );
+      return;
+    }
+
+    let totalQuestionsScore = questions.reduce(
+      (total, question) => total + question.score,
+      0
+    );
+
+    if (totalQuestionsScore !== parseInt(maxScore)) {
+      showSnackbar(
+        "The total question scores must equal the maximum score",
+        SNACKBAR_VARIANTS.ERROR
+      );
+      return;
+    }
     setLoading(true);
     try {
       await updateAssessment(id as string, examId as string, {
