d23c1532e2e44656d841912c050e71f0f53ef3d4
lTomaas <psilva@fi.uba.ar>
2025-05-23 14:43:42 -0300

    feat(pin-verification): add Google logo

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -3,5 +3,6 @@ export const images = {
     book: require("./images/books.png"),
     notFoundProfile: require("./images/not_found_profile.png"),
     icon: require("./images/icon.png"),
+    google: require("./images/google.png"),
   };
   
